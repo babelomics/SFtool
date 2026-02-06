be302661e48ab4bad8f7d3570ae45ef6a884cfba
Javier Perez-Florido <javier.perez.florido.sspa@juntadeandalucia.es>
2026-02-06 12:29:38 +0100

    Attributes added to Sample Context class for the pharmacogenetic step (#32)

--- modules/context.py
+++ modules/context.py
@@ -43,8 +43,10 @@ class SampleContext:
         self.vcf_outputs: Dict[str, Path | Dict[str, Path]] = {
             "normalized": None,
             "intersected": {},
-            "genebe_annotated": {}
+            "genebe_annotated": {},
+            "PGx_preprocessed": None
         }
+        self.reports: dict = {}
         self.variant_collections: dict = {
             "PR": {
                 "snv_indels_genebe_clinvar": {},
@@ -53,6 +55,9 @@ class SampleContext:
                 "snv_indels_genebe_clinvar": {},
                 "STRs": {},
                 "SMN1_copy": {},
+            },
+            "PGx": {
+                "pharmCAT_variants": {}
             }
         }
         self.variant_selection: dict = {
@@ -63,6 +68,9 @@ class SampleContext:
                 "snv_indels_genebe_clinvar": {},
                 "STRs": {},
                 "SMN1_copy": {},
+            },
+            "PGx": {
+                "pharmCAT_variants": {}
             }
         }
         self.results: Dict[str, dict] = {}
