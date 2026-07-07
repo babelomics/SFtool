1ca26f0616af6e129b1f8935ae46ca7c2c6a60b9
Javier Perez-Florido <javier.perez.florido.sspa@juntadeandalucia.es>
2026-07-07 13:04:53 +0200

    refactor: remove legacy function that saves intermediate results in TSV files (#42)

--- sftool/utils/utils.py
+++ sftool/utils/utils.py
@@ -1,45 +1,6 @@
 
 
-import csv
 import re
-
-def write_category_results_to_tsv(results, output_tsv):
-    """
-    Write combined results from Intervar and Clinvar to a TSV file
-
-    Args:
-        results (list): List of dictionaries with combined results
-        output_tsv (str): Output TSV file
-    """
-
-    with open(output_tsv, "w", newline="") as tsv_file:
-        fieldnames = ["Variant", "Gene", "Genotype", "Consequence", "rs", "Transcript", "HGVSC", "HGVSP", "GeneBe ACMG Classification", "GeneBe ACMG criteria", "Clinvar Clinical Significance", "ReviewStatus", "ClinvarSummary", "ClinvarID", "Orpha"]
-        writer = csv.DictWriter(tsv_file, fieldnames=fieldnames, delimiter="\t")
-
-        writer.writeheader()
-
-        for variant, info_variant in results.items():
-            for info in info_variant:  # iterate over each entry of genebe
-                # Be sure that info dictionary has all necessary keys
-                row = {
-                    "Variant": variant,
-                    "Gene": info.get("Gene", ""),
-                    "Genotype": info.get("Genotype", ""),
-                    "Consequence": info.get("Consequence", "-"),
-                    "rs": info.get("rs", ""),
-                    "Transcript": info.get("Transcript",""),
-                    "HGVSC": info.get("HGVSC",""),
-                    "HGVSP": info.get("HGVSP",""),
-                    "GeneBe ACMG Classification": info.get("GeneBeClassification", ""),
-                    "GeneBe ACMG criteria": info.get("ACMG_criteria",""),
-                    "Clinvar Clinical Significance": info.get("ClinvarClinicalSignificance", "-"),
-                    "ReviewStatus": info.get("ReviewStatus", "-"),
-                    "ClinvarSummary": info.get("ClinvarSummary", "-"),
-                    "ClinvarID": info.get("ClinvarID", "-"),
-                    "Orpha": info.get("Orpha", "")
-                }
-
-                writer.writerow(row)
 
 
 def get_clinvar_main_gene(variant_name):
