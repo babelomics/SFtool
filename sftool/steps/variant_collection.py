944272ce554e5b5416db9c31fb63799fe0eec060
Javier Perez-Florido <javier.perez.florido.sspa@juntadeandalucia.es>
2026-07-07 13:09:10 +0200

    refactor: function that combines genebe and clinvar results is moved to geneBe_utils function (#42)

--- sftool/steps/variant_collection.py
+++ sftool/steps/variant_collection.py
@@ -1,6 +1,5 @@
 from sftool.core.context import ExecutionContext
-from sftool.utils.geneBe_utils import parse_genebe_output
-from sftool.utils.utils import combine_genebe_clinvar_results
+from sftool.utils.geneBe_utils import parse_genebe_output, combine_genebe_clinvar_results
 from sftool.STRipy.STR_collection import STR_collection
 from sftool.SMAca.SMN1_collection import SMN1_collection
 from sftool.utils.pharmcat_utils import pharmCAT_collection
